e676489201554fdff980fb5a27386e66c9d836f7
Seneca Saunders <seneca.saunders@madetech.com>
2023-04-17 22:47:40 +0100

    add form to react app

--- document-numbers/src/App.tsx
+++ document-numbers/src/App.tsx
@@ -1,28 +1,71 @@
 import React, { useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   const [text, setText] = useState('');
-
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <input type="text" value={text} onChange={e => setText(e.target.value)} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {text}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+     <form method="post" id="dnrf-form">
+      <label htmlFor="project">
+        Project
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+      </label>
+      <label htmlFor='doc_rev'>
+          Document Revision
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='doc_title'>
+          Document Title
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='purpose_of_issue'>
+          Purpose of Issue
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='asset'>
+          Asset Class
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='information_type'>
+          Information Type
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='discipline'>
+          Discipline
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='suitability'>
+          Suitability
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='security_class'>
+          TfL Security Classificaton
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='project_stage'>
+          Project Stage
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='handover'>
+          Handover Information
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='hs_file'>
+        Health &amp; Safety File
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='requested_by'>
+          Requested by
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+        <label htmlFor='project_email'>
+          Project Email Address
+          <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        </label>
+     </form>
+      
+    </main>
   );
 }
 
